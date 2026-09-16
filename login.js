09177171d233a535a3ebece4b8e06a84b1346304
DangTanPhat <phat.2474802010292@vanlanguni.vn>
2026-09-16 15:55:17 +0700

    Create login.js

---
+++ login.js
@@ -0,0 +1,11 @@
+function checkLogin(username, password) {
+  if (username === 'admin' && password === '123') {
+    return true;
+  }
+  return false;
+}
+
+// Export để dùng trong Node.js (Jest)
+if (typeof module !== 'undefined' && module.exports) {
+  module.exports = checkLogin;
+}
